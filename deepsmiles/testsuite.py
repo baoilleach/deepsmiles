b2462b6ac3097502fcb7b5f54a8d075ba6eb8991
Noel O'Boyle <baoilleach@gmail.com>
2020-01-27 20:48:25 +0000

    Ensure that bracket notation for ring closures is supported fully

--- deepsmiles/testsuite.py
+++ deepsmiles/testsuite.py
@@ -150,5 +150,13 @@ class Decoding(unittest.TestCase):
         dsmi = converter.decode("C")
         self.assertEqual("C", dsmi)
 
+    def testRoundTripRingClosures(self):
+        smi = "C%(1)C%(2)C%(3)C%(4)C%(5)C%(6)C%(7)C%(8)C%(9)C%(10)C%(11)C%(12)C%(13)C%(14)C%(15)C%(16)C%(17)C%(18)C%(19)C%(20)C%(21)C%(22)C%(23)C%(24)C%(25)C%(26)C%(27)C%(28)C%(29)C%(30)C%(31)C%(32)C%(33)C%(34)C%(35)C%(36)C%(37)C%(38)C%(39)C%(40)C%(41)C%(42)C%(43)C%(44)C%(45)C%(46)C%(47)C%(48)C%(49)C%(50)C%(51)C%(52)C%(53)C%(54)C%(55)C%(56)C%(57)C%(58)C%(59)C%(60)C%(61)C%(62)C%(63)C%(64)C%(65)C%(66)C%(67)C%(68)C%(69)C%(70)C%(71)C%(72)C%(73)C%(74)C%(75)C%(76)C%(77)C%(78)C%(79)C%(80)C%(81)C%(82)C%(83)C%(84)C%(85)C%(86)C%(87)C%(88)C%(89)C%(90)C%(91)C%(92)C%(93)C%(94)C%(95)C%(96)C%(97)C%(98)C%(99)C%(100)C%(100)C%(99)C%(98)C%(97)C%(96)C%(95)C%(94)C%(93)C%(92)C%(91)C%(90)C%(89)C%(88)C%(87)C%(86)C%(85)C%(84)C%(83)C%(82)C%(81)C%(80)C%(79)C%(78)C%(77)C%(76)C%(75)C%(74)C%(73)C%(72)C%(71)C%(70)C%(69)C%(68)C%(67)C%(66)C%(65)C%(64)C%(63)C%(62)C%(61)C%(60)C%(59)C%(58)C%(57)C%(56)C%(55)C%(54)C%(53)C%(52)C%(51)C%(50)C%(49)C%(48)C%(47)C%(46)C%(45)C%(44)C%(43)C%(42)C%(41)C%(40)C%(39)C%(38)C%(37)C%(36)C%(35)C%(34)C%(33)C%(32)C%(31)C%(30)C%(29)C%(28)C%(27)C%(26)C%(25)C%(24)C%(23)C%(22)C%(21)C%(20)C%(19)C%(18)C%(17)C%(16)C%(15)C%(14)C%(13)C%(12)C%(11)C%(10)C%(9)C%(8)C%(7)C%(6)C%(5)C%(4)C%(3)C%(2)C%(1)"
+        for branches in [True, False]:
+            converter = ds.Converter(rings=True, branches=branches)
+            encoded = converter.encode(smi)
+            decoded = converter.decode(encoded)
+            self.assertTrue("%(100)" in decoded)
+
 if __name__ == "__main__":
     unittest.main()
